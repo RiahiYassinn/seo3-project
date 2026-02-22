7193baa13ba6c58114829c94c0d06440f40d5423
Riahi Yassine <yassine.riahi@esprit.tn>
2026-02-22 12:32:56 +0100

    setup turborepo

--- services/nlp-service/app/config.py
+++ services/nlp-service/app/config.py
@@ -22,6 +22,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = "../../.env"
         case_sensitive = True
+        extra = "ignore"  # Allow extra env vars from shared .env file
 
 
 settings = Settings()
